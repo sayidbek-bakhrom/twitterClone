51b2a31b9bda089023a4bef91c76d7200f8db0eb
Sayidbek <sayidbekbakhrom@gmail.com>
2023-03-07 19:43:18 +0500

    creat users, profile and follows

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-
+from .models import Profile
 
 # unregister groups
 admin.site.unregister(Group)
 
 
-class UserAdmin
+# mix profile info into user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+# unregister initial user
+admin.site.unregister(User)
+# register user and profile
+admin.site.register(User, UserAdmin)
