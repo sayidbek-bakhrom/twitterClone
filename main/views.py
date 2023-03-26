dc7dfa44be73bb619ea0d30d279eb862cade2bca
Sayidbek <sayidbekbakhrom@gmail.com>
2023-03-26 23:38:04 +0500

    add update_user page

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from .forms import TweetForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -96,3 +97,18 @@ def register_user(request):
             messages.success(request, f"{first_name} have registered successfully!")
             return redirect("home")
     return render(request, "register.html", {"form": form})
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        form = SignUpForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+            login(request, current_user)
+            messages.success(request, "Updated successfully!")
+            return redirect("home")
+        return render(request, "update_user.html", {"form": form})
+    else:
+        messages.info(request, "You must be logged in to update profile!")
+    return render(request, "update_user.html")
